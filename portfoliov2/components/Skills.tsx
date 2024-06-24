bbf5b2a5c2c120608f219f6f3f778b3d65f1c468
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-23 19:25:42 -0700

    Worked on width sizing for the skills section

--- portfoliov2/components/Skills.tsx
+++ portfoliov2/components/Skills.tsx
@@ -21,8 +21,8 @@ const Skills = () =>{
             Build lamps <br /> the right way
           </motion.h1>
         </LampContainer>
-        <div className="">
-        <div className=" w-full relative max-w-xs">
+        <div className="flex justify-center">
+        <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] w-full relative max-w-screen-md">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
           <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
   

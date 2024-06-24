a081b426c8443604675f0bb51cb9773be187b8e4
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-23 20:02:55 -0700

    removed the lamp from the skills section

--- portfoliov2/components/Skills.tsx
+++ portfoliov2/components/Skills.tsx
@@ -1,26 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { LampContainer } from "./ui/Lamp";
 import { Meteors } from "./ui/meteors";
 
 const Skills = () =>{
     return(
       <div>
-        <LampContainer>
-          <motion.h1
-            initial={{ opacity: 0.5, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="mt-2 bg-gradient-to-br from-slate-300 to-slate-500 py-1 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-          >
-            Build lamps <br /> the right way
-          </motion.h1>
-        </LampContainer>
         <div className="flex justify-center dark:bg-black-100 bg-white">
         <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] w-full relative max-w-screen-md">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />

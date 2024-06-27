903fc8a22a4473cf30e779a1cda1c31ed012044d
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-26 21:36:56 -0700

    Imported missing Files and did more work on the skills section

--- portfoliov2/components/Skills.tsx
+++ portfoliov2/components/Skills.tsx
@@ -13,18 +13,13 @@ const Skills = () =>{
         <div className="flex justify-center dark:bg-black-100 bg-white">
         <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] w-full relative max-w-screen-md">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-          <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-center">
   
             <h1 className="font-bold md:text-3xl lg:text-4xl text-white mb-4 relative z-50">
               Languages and Frameworks
             </h1>
   
-            <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-              I don&apos;t know what to write so I&apos;ll just paste something
-              cool here. One more sentence because lorem ipsum is just
-              unacceptable. Won&apos;t ChatGPT the shit out of this.
-            </p>
-            <div>
+            <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
               {skills_Icons.map((image, index) => (
                 <SkillIconProvider 
                 key={index}

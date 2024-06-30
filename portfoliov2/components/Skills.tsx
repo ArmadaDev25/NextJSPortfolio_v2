fb755499c96c12559f055d876417e1a69e4f67a1
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-30 14:28:49 -0700

    Updated padding for the skills section

--- portfoliov2/components/Skills.tsx
+++ portfoliov2/components/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () =>{
     
   return(
       <div>
-        <div className="flex justify-center dark:bg-black-100 bg-white">
+        <div className=" py-10 flex justify-center dark:bg-black-100 bg-white">
         <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] w-full relative max-w-screen-md">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
           <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-center">

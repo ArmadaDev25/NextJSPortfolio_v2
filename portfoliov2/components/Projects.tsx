c066a1c466156bb7c6920389d5e170848b810638
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-30 20:57:17 -0700

    worked on projects and skills sections

--- portfoliov2/components/Projects.tsx
+++ portfoliov2/components/Projects.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Projects = () =>{
     return(
-        <div>
-            <h1 className="heading realtive">
+        <div className="py-10 dark:bg-black-100 bg-white">
+            <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
                 My Projects
             </h1>
 
--- portfoliov2/components/Skills.tsx
+++ portfoliov2/components/Skills.tsx
@@ -9,14 +9,19 @@ const Skills = () =>{
   
     
   return(
-      <div>
-        <div className=" py-10 flex justify-center dark:bg-black-100 bg-white">
+      <div className="py-10 dark:bg-black-100 bg-white">
+        
+        <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
+          Languages and Frameworks
+        </h1>
+        
+        <div className=" flex justify-center ">
         <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] w-full relative max-w-screen-md">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
           <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-center">
             <Meteors number={20} />
   
-            <h1 className="font-bold md:text-3xl lg:text-4xl text-white mb-4 relative z-50">
+            <h1 className="font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
               Languages and Frameworks
             </h1>
   

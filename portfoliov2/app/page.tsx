0afb58f0b4ef2325f54cfc1d2a65f89e06d511d3
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-06-30 19:35:12 -0700

    added the skills section to the homepage

--- portfoliov2/app/page.tsx
+++ portfoliov2/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import { Projects } from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Image from "next/image";
 
@@ -13,9 +14,13 @@ export default function Home() {
 
       </div>
       
-    </main>
     
+    
+    
+    
+    </main>
     <Skills />
+    <Projects />
     </div>
   );
 }
---
+++ portfoliov2/components/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Projects = () =>{
+    return(
+        <div>
+            <h1 className="heading realtive">
+                My Projects
+            </h1>
+
+        </div>
+    )
+
+}

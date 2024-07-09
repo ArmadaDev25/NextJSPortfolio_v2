4d1282eb8f329d6da69a1716a2d3d31fac2ceb86
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-07-08 21:58:02 -0700

    Update Projects.tsx

--- portfoliov2/components/Projects.tsx
+++ portfoliov2/components/Projects.tsx
@@ -11,21 +11,22 @@ export const Projects = () =>{
             <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
                 My Projects
             </h1>
-            <div className="flex flex-wrap gap-10 items-center justify-center">
+            <div className="flex flex-wrap gap-10  justify-center">
                 {myWebProjects.map(({name, livelnk, des, githublnk, png, gitlogo }, index) => (
                     
                     <div className="">
                         <div className=" w-full relative max-w-xs">
                         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-                            <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+                            <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
                             <div>
+                            <h1 className="md:text-2xl text-base font-bold py-2">{name}</h1>
                             <Image src={png} width={290} height= {290} alt='Page Picture'/>
-                            <h1 className="md:text-2xl text-base font-bold">{name}</h1>
-                            <p className=" md:text-base text-sm">
+                            
+                            <p className=" md:text-base text-sm py-2">
                                 {des}
 
                             </p>
-                            <div className="flex flex-wrap gap-3 justify-end items-center">
+                            <div className="flex flex-wrap gap-3 justify-end items-center py-2">
                             <Tooltip content="Link Source Code">
                                 <Image src={gitlogo} width={50} height= {40} alt='Repo Link'/>
                             </Tooltip>

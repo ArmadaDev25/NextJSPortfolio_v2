a88184c217aea8c0877195406a4746e85b6e4f42
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-07-08 18:41:19 -0700

    Added the Navbar to the top

--- portfoliov2/app/page.tsx
+++ portfoliov2/app/page.tsx
@@ -2,10 +2,12 @@ import Hero from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+    <Navbar />
     <main className= "relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
 
       <div className="max-w-7xl w-full">

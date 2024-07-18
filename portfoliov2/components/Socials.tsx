5d6ff55610c91ecdb64eac21e1f63f9a39352498
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-07-17 21:17:54 -0700

    Update Socials.tsx

--- portfoliov2/components/Socials.tsx
+++ portfoliov2/components/Socials.tsx
@@ -1,11 +1,20 @@
 
+"use client";
 import Image from "next/image";
+import { socials } from "@/public/Data";
 export const Socials = () =>{
     return(
         <div className="py-10 dark:bg-black-100 bg-white">
             <h1>
                 My Socials
             </h1>
+            <div>
+                {socials.map ((socLnk, index) => (
+                    <Image src={socLnk.icon} width={50} height= {40} alt='Social Link'/>
+                    
+
+                ))}
+            </div>
             
         
         </div>

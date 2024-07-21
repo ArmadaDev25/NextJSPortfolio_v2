99d1232001bec67720da6d7c38cc0663586c9906
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-07-20 23:55:05 -0700

    Update Socials.tsx

--- portfoliov2/components/Socials.tsx
+++ portfoliov2/components/Socials.tsx
@@ -8,7 +8,7 @@ export const Socials = () =>{
             <h1>
                 My Socials
             </h1>
-            <div>
+            <div className="flex">
                 {socials.map ((socLnk, index) => (
                     <Image src={socLnk.icon} width={50} height= {40} alt='Social Link'/>
                     

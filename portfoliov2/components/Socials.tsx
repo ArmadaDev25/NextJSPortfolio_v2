77dfe012847d000152a633af6f6aa6dddd6cddd6
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-07-22 21:59:59 -0700

    Update Socials.tsx

--- portfoliov2/components/Socials.tsx
+++ portfoliov2/components/Socials.tsx
@@ -5,7 +5,7 @@ import { socials } from "@/public/Data";
 export const Socials = () =>{
     return(
         <div className="py-10 dark:bg-black-100 bg-white">
-            <h1>
+            <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
                 My Socials
             </h1>
             <div className="flex gap-10">

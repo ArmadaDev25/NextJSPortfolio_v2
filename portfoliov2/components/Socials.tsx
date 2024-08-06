175bb9863a9df78d2fcd9d731accce6a3d4d1a0b
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-08-05 17:15:57 -0700

    Update Socials.tsx

--- portfoliov2/components/Socials.tsx
+++ portfoliov2/components/Socials.tsx
@@ -6,10 +6,13 @@ import { Tooltip } from "flowbite-react";
 export const Socials = () =>{
     return(
         <div className="py-10 dark:bg-black-100 bg-white">
-            <div className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]">
-            <div>
-                <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
+            <div className="flex md:flex-row flex-col justify-between items-center px-20 mt-40 w-full z-[20]">
+            <div className="flex flex-col items-center justify-around ">
+                <h1 >
                     My Socials
+                </h1>
+                <h1>
+                    Click the Icons To View My Socials
                 </h1>
                 
                 <div className="flex gap-10">

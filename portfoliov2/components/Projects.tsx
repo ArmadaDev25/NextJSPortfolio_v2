048473ce23cc5c430d8e2f9d7faca04dcbf5c7f1
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-08-13 20:35:50 -0700

    More work on Spacing

--- portfoliov2/components/Projects.tsx
+++ portfoliov2/components/Projects.tsx
@@ -7,7 +7,8 @@ import { Tooltip } from "flowbite-react";
 
 export const Projects = () =>{
     return(
-        <div className=" py-20 dark:bg-black-100 bg-white" id="projects">
+        <div className="pt-20 dark:bg-black-100 bg-white" id="projects">
+        <div className="py-20" >
             <h1 className="flex justify-center font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50">
                 My Projects
             </h1>
@@ -44,6 +45,7 @@ export const Projects = () =>{
                         </div>
                         </div>
                     </div>
+            
                     
                     
 
@@ -55,6 +57,7 @@ export const Projects = () =>{
 
             </div>
 
+        </div>
         </div>
     )
 

44bae250d72ea72998c9160d8abd21cc2b4751de
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-08-14 20:32:32 -0700

    Update Socials.tsx

--- portfoliov2/components/Socials.tsx
+++ portfoliov2/components/Socials.tsx
@@ -5,10 +5,11 @@ import { socials } from "@/public/Data";
 import { Tooltip } from "flowbite-react";
 export const Socials = () =>{
     return(
-        <div className="pt-40 pb-60 dark:bg-black-100 bg-white" >
-            <div className="flex md:flex-row flex-col justify-between items-center px-20 mt-40 w-full z-[20]">
+        <div className="pt-30 pb-60 dark:bg-black-100 bg-white"   >
+            <div className="pt-10 "  id="socials" ></div>
+            <div className="flex md:flex-row flex-col justify-between items-center px-20 mt-40 w-full z-[20] " id="socials">
             <div className="flex flex-col items-center justify-around ">
-                <h1 className="font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50" id="socials" >
+                <h1 className="font-bold md:text-3xl sm:text-xl lg:text-4xl text-white mb-4 relative z-50" >
                     My Socials
                 </h1>
                 <h1>
@@ -17,9 +18,11 @@ export const Socials = () =>{
                 
                 <div className="flex gap-10 py-5">
                     {socials.map ((socLnk, index) => (
+                        <a href={socLnk.link}>
                         <div className=" hover:opacity-50 ">
                         <Image src={socLnk.icon} width={60} height= {60} alt='Social Link'/>
                         </div>
+                        </a>
                         
 
                     ))}

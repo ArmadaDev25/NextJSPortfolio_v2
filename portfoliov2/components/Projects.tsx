eb16b7e2b169743e0ac472daf2df55dffebcb4ab
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-08-18 18:35:46 -0700

    Attempted to fix the key issue

--- portfoliov2/components/Projects.tsx
+++ portfoliov2/components/Projects.tsx
@@ -16,7 +16,7 @@ export const Projects = () =>{
                 {myWebProjects.map(({name, livelnk, des, githublnk, png, gitlogo }, index) => (
                     
                     <div className="">
-                        <div className=" w-full relative max-w-xs">
+                        <div className=" w-full relative max-w-xs" key = {index}>
                         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
                             <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
                             <div>
